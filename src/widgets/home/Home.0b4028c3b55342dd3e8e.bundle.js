06b39949f2931a50435e4646cbd720b39b0c8c45
Paul Blew <paul.blew@gmail.com>
2020-07-07 17:33:29 +0100

    Removed sourceMappingURL references

--- src/widgets/home/Home.0b4028c3b55342dd3e8e.bundle.js
+++ src/widgets/home/Home.0b4028c3b55342dd3e8e.bundle.js
@@ -7,4 +7,3 @@
  * 
  */
 (window.dojoWebpackJsonpweb_settlers=window.dojoWebpackJsonpweb_settlers||[]).push([["src/widgets/home/Home"],{273:function(e,o,t){e.exports={" _key":"web-settlers/Home",root:"Home-m__root__2249f41lPCV"}},286:function(e,o,t){"use strict";t.r(o);var s=t(20),r=t(273);const n=Object(s.a)();o.default=n(function(){return Object(s.g)("h1",{classes:[r.root]},"Home Page")})}}]);
-//# sourceMappingURL=Home.0b4028c3b55342dd3e8e.bundle.js.map
