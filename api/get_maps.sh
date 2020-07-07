11c3a99155c1d52737217d79f7502b79176a446d
Paul Blew <paul.blew@gmail.com>
2020-07-07 17:24:41 +0100

    Removed gzipped versions of api data and `.map` files

--- api/get_maps.sh
+++ api/get_maps.sh
@@ -6,14 +6,12 @@ rm -rf map/*
 echo -n Retrieving map list...
 curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map | jq . > maps
 MAPS=$(grep '"id"' maps | cut -d '"' -f 4)
-gzip -k9 maps
 echo Done
 
 for map in ${MAPS}
 do
 	echo -n Retrieving map ${map}...
 	curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map/${map} -o map/${map}
-	gzip -k9 map/${map}
     echo Done
 done
 
