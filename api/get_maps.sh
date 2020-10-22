53c458c79f5c151549143d7e2622672c3edbdf5e
Paul Blew <paul.blew@gmail.com>
2020-10-22 18:10:17 +0100

    Updated api data

--- api/get_maps.sh
+++ api/get_maps.sh
@@ -2,6 +2,7 @@
 
 rm -f maps
 rm -rf map/*
+rm -rf preview/*
 
 echo -n Retrieving map list...
 curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map | jq . > maps
@@ -11,7 +12,7 @@ echo Done
 for map in ${MAPS}
 do
 	echo -n Retrieving map ${map}...
+	curl -H "Accept: image/png" -s -S http://localhost:8080/websettlers/data/preview/${map} -o preview/${map}
 	curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map/${map} -o map/${map}
-    echo Done
+  echo Done
 done
-
