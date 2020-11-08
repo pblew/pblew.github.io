8b3e2d95d8b855d1a45eaab18d1b75c4bb07fddc
Paul Blew <paul.blew@gmail.com>
2020-11-08 13:33:21 +0000

    Updated to latest build

--- api/get_maps.sh
+++ api/get_maps.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-rm -f maps
+rm -f maps.json
 rm -rf map/*
 rm -rf preview/*
 
 echo -n Retrieving map list...
-curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map | jq . > maps
-MAPS=$(grep '"id"' maps | cut -d '"' -f 4)
+curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/maps.json | jq . > maps.json
+gzip -k9 maps.json
+MAPS=$(grep '"id"' maps.json | cut -d '"' -f 4)
 echo Done
 
 for map in ${MAPS}
 do
 	echo -n Retrieving map ${map}...
-	curl -H "Accept: image/png" -s -S http://localhost:8080/websettlers/data/preview/${map} -o preview/${map}
-	curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map/${map} -o map/${map}
+	curl -H "Accept: image/png" -s -S http://localhost:8080/websettlers/data/preview/${map}.png -o preview/${map}.png
+	curl -H "Accept: application/json" -s -S http://localhost:8080/websettlers/data/map/${map}.json -o map/${map}.json
+	gzip -k9 map/${map}.json
   echo Done
 done
