1f81daae976696c201cfd9044b2544ddc3b8d98a
Paul Blew <paul.blew@gmail.com>
2021-07-03 17:16:17 +0100

    `.bundle.{j|cs}s` resource are now compressed

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo -n Removing source maps...
 find . -name '*.map' -exec rm -f {} \;
 find . -name '*.bundle.js' -exec sed -i '/\/\/# sourceMappingURL=/d' {} \;
 find . -name '*.bundle.css' -exec sed -i '/\/*# sourceMappingURL=/d' {} \;
 sed -i '/\.map":/d' manifest.json
+echo Done
+
+echo -n Compressing resources...
+find . -name '*.bundle.js' -exec gzip -k9 {} \;
+find . -name '*.bundle.css' -exec gzip -k9 {} \;
+echo Done
