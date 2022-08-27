c7a2c820ff8d12f5614aebc7a7366f7df1091b7c
Paul Blew <paul.blew@gmail.com>
2022-08-27 18:35:34 +0100

    Updated to latest build

--- 4.339cb30463c73091dcea.bundle.js
+++
@@ -1,9 +0,0 @@
-/*!
- * 
- * [Dojo](https://dojo.io/)
- * Copyright [JS Foundation](https://js.foundation/) & contributors
- * [New BSD license](https://github.com/dojo/meta/blob/master/LICENSE)
- * All rights reserved
- * 
- */
-(window.dojoWebpackJsonpweb_experiments=window.dojoWebpackJsonpweb_experiments||[]).push([[4],{388:function(e,o,s){"use strict";s.r(o),o.default={requiredMessage:"请选择一个值。"}}}]);
