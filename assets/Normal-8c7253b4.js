555bffd3a5f34d926ca6ca6150c6dedd0696ed92
Paul Blew <paul.blew@gmail.com>
2023-05-27 18:36:45 +0100

    Updated to latest build

---
+++ assets/Normal-8c7253b4.js
@@ -0,0 +1,1 @@
+import{f as a}from"./Colours-ced390c5.js";function u(r,s,t){const o=1/Math.sqrt(r*r+s*s+t*t),e=a(r*o,s*o,t*o);return{dx:e[0],dy:e[1],dz:e[2],asVec3:()=>e}}export{u};
---
+++ assets/Normals-f722241c.js
@@ -0,0 +1,1 @@
+import{F as o,I as a}from"./Colours-ced390c5.js";class l extends o{constructor(t){super(t,a.VEC3)}getNormal(t){return this.itemAt(t)}setNormal(t,e,s,r){this.setItem(t,e,s,r)}}export{l as N};
