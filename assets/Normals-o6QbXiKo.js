7ae84ea480caee96865a5c0878be4588466026ac
Paul Blew <paul.blew@gmail.com>
2023-12-23 17:49:51 +0000

    Updated to latest build

---
+++ assets/Normals-o6QbXiKo.js
@@ -0,0 +1,1 @@
+import{F as o,I as a}from"./Colours-A6vT7UvC.js";class l extends o{constructor(t){super(t,a.VEC3)}getNormal(t){return this.itemAt(t)}setNormal(t,e,s,r){this.setItem(t,e,s,r)}}export{l as N};
